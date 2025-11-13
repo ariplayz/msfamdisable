9745926ee5d225dd6b81f8487c5261da379729ec
Ari Cummings <ari@aricummings.com>
2025-11-13 14:24:10 -0800

    start, and add functions

---
+++ Command.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using System.Management.Automation;
+
+namespace msfamdisable_win11;
+public class Command
+{ 
+    private PowerShell cmd = PowerShell.Create();
+    private Process proc = new Process();
+    
+    public void Set(string command)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Setting command: " + command);
+        Console.WriteLine("");
+        cmd.AddScript(command);
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Running command... " + cmd);
+        Console.WriteLine("");
+        var results = cmd.Invoke();
+
+        // Display results
+        foreach (var result in results)
+        {
+            Console.WriteLine(result);
+        }
+        
+        cmd.AddScript("");
+
+    }
+
+    public void RunProcess(string process) 
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Running process: " + process);
+        Console.WriteLine("");
+        proc.StartInfo.FileName = process;
+        proc.Start();
+    }
+
+    public void Remove(string path)
+    {
+        Set("Remove-Item " + path + " -Recurse -Force -Confirm:$false");
+        Console.WriteLine("");
+        Console.WriteLine("Removing Item: " + path);
+        Console.WriteLine("");
+        Run();
+    }
+
+    public void TakeOwn(string path)
+    {
+        Set($"$ACL = Get-ACL '{path}'");
+        Run();
+
+        Set("$Group = New-Object System.Security.Principal.NTAccount('Builtin', 'Administrators')");
+        Run();
+
+        Set("$ACL.SetOwner($Group)");
+        Run();
+
+        Set($"Set-Acl -Path '{path}' -AclObject $ACL");
+        Console.WriteLine("");
+        Console.WriteLine("Taking ownership of: " + path);
+        Console.WriteLine("");
+        Run();
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace msfamdisable_win11;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        
+        Console.WriteLine("MSFamDisable 1.0");
+        Console.WriteLine("by Ari Cummings");
+        Console.WriteLine("");
+        Console.WriteLine($"Running on user: {userName}");
+        Console.WriteLine("This will disable Family Features by removing the executables and libraries");
+        Console.WriteLine("Continue? (Y/N)");
+        string gostring = Console.ReadLine();
+        if (gostring.ToLower() != "y")
+        {
+            Console.WriteLine("Exiting...");
+            return 1;
+        }
+
+        Dictionary<int, string> paths = new Dictionary<int, string>();
+        paths.Add(1, @"C:\Program Files\WindowsApps\microsoftcorporationii.microsoftfamily_0.2.40.0_x64__8wekyb3d8bbwe\FamilyHub.exe");
+        paths.Add(2, @"C:\Program Files\WindowsApps\AppleInc.iCloud_15.5.23.0_x64__nzyj5cx40ttqa\iCloud\WebView2\msedge.dll");
+        
+        
+        
+        
+        return 0;
+    }
+}
